a6aaab1132fa165d38692c8220d363c0a410520a
daniedev <a.danielpf21@gmail.com>
2022-11-27 17:01:11 +0530

    Chat Screen with Animations

---
+++ app/src/main/java/github/daniedev/learncompose/ui/sample/chatscreen/Conversation.kt
@@ -0,0 +1,20 @@
+package github.daniedev.learncompose.ui.sample.chatscreen
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+
+@Preview(showBackground = true)
+@Composable
+fun Conversation(@PreviewParameter(ConversationProvider::class) messages: List<Message>) {
+    LazyColumn {
+        messages.map { item { MessageCard(it) } }
+    }
+}
+
+class ConversationProvider: PreviewParameterProvider<List<Message>> {
+    override val values: Sequence<List<Message>>
+        get() = sequenceOf(SampleData.conversationSample)
+}
---
+++ app/src/main/java/github/daniedev/learncompose/ui/sample/chatscreen/MessageCard.kt
@@ -0,0 +1,77 @@
+package github.daniedev.learncompose.ui.sample.chatscreen
+
+import android.content.res.Configuration
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
+import github.daniedev.learncompose.R
+
+@Preview(name = "light-mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "dark mode",
+    showBackground = true
+)
+@Composable
+fun MessageCard(@PreviewParameter(MessageCardProvider::class) message: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact Profile Picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+        Column(
+            modifier = Modifier.clickable { isExpanded = !isExpanded }
+        ) {
+            Text(
+                text = message.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = message.body,
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1
+                )
+            }
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+class MessageCardProvider: PreviewParameterProvider<Message> {
+    override val values: Sequence<Message>
+        get() = sequenceOf(SampleData.conversationSample.get(0))
+}
